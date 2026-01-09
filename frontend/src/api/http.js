fae6dfc752bab30c7f4fcb8a79aff1456d4c440f
Advolve24 <web@advolve.in>
2026-01-10 00:12:28 +0530

    Frontend and backend update

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,9 +1,10 @@
 // utils/api.js
 import axios from "axios";
 
-const baseURL = import.meta.env.DEV
-  ? "http://localhost:5000/api"
-  : "https://gulposh-booking-system.onrender.com/api";
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "http://localhost:5000/api";
+
 
 export const api = axios.create({
   baseURL,
