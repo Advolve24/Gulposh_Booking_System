dd4dc06d148ad8948d163f68231a9216d92d0a52
Advolve24 <web@advolve.in>
2026-01-14 11:12:55 +0530

    frontend Room page book now model added

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -8,6 +8,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Separator } from "@/components/ui/separator";
+import { ArrowLeft } from "lucide-react";
+
 
 import {
   Select,
