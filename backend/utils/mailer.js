c87bdfbe3b83cfc5f66f5477bee4dfb875984762
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-11 12:25:44 +0530

    up

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -443,7 +443,7 @@ stroke-linejoin="round"
 style="margin-left:10px;">
 
 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+<path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
 
 </svg></b>
 </td>
