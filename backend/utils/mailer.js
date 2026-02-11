bf05ce828b2f31f3fc70b61f24c91e5ac476804c
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-11 12:21:29 +0530

    up

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -59,7 +59,7 @@ export const sendBookingConfirmationMail = async ({
     room.location || booking.location || "Karjat, Maharashtra, India";
 
   return transporter.sendMail({
-    from: `"Gulposh Villa" <${process.env.SMTP_USER}>`,
+    from: `"Villa Gulposh" <${process.env.SMTP_USER}>`,
     to,
     subject: `Booking Confirmed – ${room.name}`,
     html: `
@@ -431,7 +431,21 @@ border-radius:12px;
 <td style="padding:14px;">
 <b>📶 WiFi</b><br><br>
 GrandRepose_Guest<br>
-Pass: <b>Welcome@2026</b>
+Pass: <b>Welcome@2026 <svg xmlns="http://www.w3.org/2000/svg"
+width="16"
+height="16"
+viewBox="0 0 24 24"
+fill="none"
+stroke="#6b7280"
+stroke-width="2"
+stroke-linecap="round"
+stroke-linejoin="round"
+style="margin-left:10px;">
+
+<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+
+</svg></b>
 </td>
 </tr>
 
